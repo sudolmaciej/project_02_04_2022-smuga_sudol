7cf9027bc34fb4722ced398291312c9efefd85b2
sudolmaciej <101933820+sudolmaciej@users.noreply.github.com>
2022-06-17 22:32:59 +0200

    Update main.c
    komentarze

--- main.c
+++ main.c
@@ -16,13 +16,16 @@ int main() {
     int n;
     scanf("%d",&n);
     generowanie_liczb(n);
+    //Wczytanie szukanej liczby
 
     printf("Podaj szukana liczbe: ");
     int szukana;
     scanf("%d",&szukana);
+    //Otwarcie pliku w trybie odczytywania
     FILE *fp;
     fp = fopen("xd.txt", "r");
     int x;
+    //tworzenie tablicy dynamicznej
     int *tab2 = NULL;
     tab2 = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++)
@@ -30,7 +33,9 @@ int main() {
         fscanf(fp,"%d",&x);
         tab2[i] = x;
     }
-
+    //petla odczytujaca liczby z pliku i zapisujaca do tablicy
+    
+    //rozpoczecie mierzenia czasu
     int wynik;
     struct timeval begin, end;
     gettimeofday(&begin, 0);
@@ -38,15 +43,18 @@ int main() {
     wynik = wykladnicze_wysz(tab2,n,szukana);
 
     gettimeofday(&end, 0);
+    //zakonczenie mierzenia czasu
     long seconds = end.tv_sec - begin.tv_sec;
     long microseconds = end.tv_usec - begin.tv_usec;
     double elapsed = seconds + microseconds*1e-6;
+    //podanie wyniku wyszukiwania
     if (wynik == -1)
         printf("Szukana liczba nie znajduje sie na liscie.\n");
     else
         printf("Szukana liczba znajduje sie na pozycji: %d.\n",wynik);
 
     printf("Operacja wyszukiwania trwala: %f sekund.",elapsed);
+    //podanie wyniku wyszukiwania
 
 
 
@@ -64,15 +72,18 @@ int main() {
 }
 
 
-
+//funkcja void generujaca liczby i zapsisujaca do dynamicznej tablicy a nastepnie do pliku tekstowego
+//otwieranie pliku tekstowego w trybie zapisywania
 void generowanie_liczb(int n)
 {
 
     srand(time(NULL));
     FILE *fp;
     fp = fopen("xd.txt","w");
+    // tworzenie tablicy dynamicznej
     int *tab = NULL;
     tab = malloc(n * sizeof(int));
+    //petla generujaca liczby z zakresu od 0 do 10 000 000
     for (int i = 0; i< n; i++)
     {
         int a = rand()%1001;
@@ -80,6 +91,7 @@ void generowanie_liczb(int n)
         tab[i] = a*b;
     }
     for (int i = 0; i < n; i++)
+        //petla generujaca liczby z zakresu od 0 do 10 000 000
     {
         for (int j = 1; j < n-i; j++)
         {
@@ -92,34 +104,36 @@ void generowanie_liczb(int n)
             }
         }
     }
+    //petla generujaca liczby z zakresu od 0 do 10 000 000
     for (int i = 0; i < n; i++)
     {
         fprintf(fp,"%d\n",tab[i]);
     }
+    //zamkniecie pliku i wyzerowanie tablicy dynamicznej
     fclose(fp);
     free(tab);
     tab = NULL;
 
 }
 
-
+//algorytm wyszukiwania oparty na Exponential Search
 int wykladnicze_wysz(int tab2[],int n, int szukana)
 {
-    if (tab2[0] == szukana)
+    if (tab2[0] == szukana) //sprawdzenie czy pierwsza liczba w tablicy nie jest nasza szukana
         return 0;
-    int poz = 1;
-    while (poz < n && tab2[poz] <= szukana)
+    int poz = 1;    //pomocnicza zmienna dzieki ktorej bedziemy sprawdzac czy nasza szukana jest zmniejsza czy wieksza
+    while (poz < n && tab2[poz] <= szukana) //petla ktora mnozy pomocnicza zmienna razy 2 dopoki nie bedzie wieksza od szukanej
     {
         poz = poz *2;
     }
-    if (poz > n)
-        return binarne_wysz(tab2, poz/2, n,szukana);
+    if (poz > n)    //sprawdzenie czy pomocnicza liczba nie wykracza poza ilosc naszych liczb w tablicy
+        return binarne_wysz(tab2, poz/2, n,szukana);    //jezeli wykracza to zakres szukania binarnego jest od poz/2 do n
     else
-        return binarne_wysz(tab2, poz/2, poz,szukana);
+        return binarne_wysz(tab2, poz/2, poz,szukana);  //jezeli nie to od poz/2 do poz
 }
 
 
-int binarne_wysz(int tab2[],int left, int right, int szukana)
+int binarne_wysz(int tab2[],int left, int right, int szukana)      //algorytm wyszukiwania binarnego
 {
     //printf("left: %d right: %d",left,right);
     if (right >= left)
